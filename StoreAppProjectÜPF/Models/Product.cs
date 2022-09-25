d325ca7160f4e1837dbeb095cce0f6e98d93cde3
nurlan.shirinov1998 <nurlan.shirinov1998@gmail.com>
2022-09-26 02:38:26 +0400

    Add project files.

---
+++ StoreAppProjectÜPF/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreAppProjectÜPF.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Path { get; set; }
+    }
+}
